b95ace572fd9a13dfda130b3dda387e548bd4231
BrianC1992 <BrianC1992@users.noreply.github.com>
2016-06-05 16:18:15 -0400

    Update q5_datetime.py

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -26,5 +26,14 @@ delta = data_stop-data_start
 print(delta.days)
 
 ####c)  
-date_start = '15-Jan-1994'  
-date_stop = '14-Jul-2015'  
+from datetime import datetime
+date_format = "%d-%b-%Y"
+date_start = '15-Jan-1994'    
+date_stop = '14-Jul-2015' 
+
+data_start = datetime.strptime(date_start, date_format)
+data_stop = datetime.strptime(date_stop, date_format)
+
+delta = data_stop-data_start
+
+print(delta.days) 

0a23bb355e617be6318d299fdf7f319fae45ed71
BrianC1992 <BrianC1992@users.noreply.github.com>
2016-06-05 14:33:32 -0400

    Update q5_datetime.py

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -1,8 +1,16 @@
 # Hint:  use Google to find python function
 
 ####a) 
+from datetime import datetime
+date_format = "%m-%d-%Y"
 date_start = '01-02-2013'  
-date_stop = '07-28-2015'   
+date_stop = '07-28-2015' 
+data_start = datetime.strptime('01-02-2013', date_format)
+data_stop = datetime.strptime('07-28-2015', date_format)
+
+delta = data_stop-data_start
+
+print(delta.days)
 
 ####b)  
 date_start = '12312013'  

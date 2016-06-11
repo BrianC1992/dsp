857b6241646c418dfc982b6fb4fba91086d4ef80
BrianC1992 <BrianC1992@users.noreply.github.com>
2016-06-11 12:40:39 -0400

    Update advanced_python_csv.py

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,7 @@
-PLACE YOUR CODE HERE
+import csv
+import pandas as pd
+faculty = pd.read_csv('/home/brianc/ds/metis/prework/dsp/python/faculty.csv')
+faculty.columns = ['name','degree','title','email']
+faculty["degree"] =faculty["degree"].str.strip().str.replace('Ph.D.','PhD').str.replace('Ph.D','PhD').str.replace('PhD', 'Ph.D.')
+faculty["degree"]=faculty["degree"].str.strip().str.replace('ScD','Sc.D.').str.replace('MS','M.S.')
+faculty.email.to_csv('emails.csv', header=False, index=False)

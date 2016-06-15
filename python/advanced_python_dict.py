bbb5b7df01b009384079f08132128287b39744c5
BrianC1992 <BrianC1992@users.noreply.github.com>
2016-06-15 00:02:43 -0400

    Update advanced_python_dict.py

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -1,1 +1,18 @@
-PLACE YOUR CODE HERE
+# Qestion 6
+import pandas as pd
+import numpy as np
+import re
+faculty = pd.read_csv('/home/brianc/ds/metis/prework/dsp/python/faculty.csv')
+faculty.columns = ['name','degree','title','email']
+faculty["degree"] =faculty["degree"].str.strip().str.replace('Ph.D.','PhD').str.replace('Ph.D','PhD').str.replace('PhD', 'Ph.D.')
+faculty["degree"]=faculty["degree"].str.strip().str.replace('ScD','Sc.D.').str.replace('MS','M.S.')
+firstLast =(r'(.+)\s(.+)')
+faculty[['FirstName','LastName']]=faculty['name'].str.extract(firstLast, expand = True)
+faculty.drop(['name','FirstName'], axis=1, inplace=True)
+mydict = {}
+for x in range(len(faculty)):
+    currentid = faculty.iloc[x,3]
+    currentvalue = faculty.iloc[x,0:3]
+    mydict.setdefault(currentid, [])
+    mydict[currentid].append(currentvalue)
+mydict

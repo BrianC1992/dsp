c1763b0cdefdeb97f02c877f0997c683c4c26096
BrianC1992 <BrianC1992@users.noreply.github.com>
2016-06-15 23:24:29 -0400

    Update advanced_python_dict.py

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -33,5 +33,20 @@ faculty.drop(['name'], axis=1, inplace=True)
 faculty = faculty[['FirstName','LastName','degree','title','email']]
 mydict = faculty.set_index(['FirstName','LastName']).T.to_dict()
       
-first3pairs = {k: mydict[k] for k in list(mydict.keys())[:3]}
+first3pairs = {k: mydict[k] for k in sorted(mydict.keys())[:3]}
+first3pairs
+
+#Question 8
+faculty = pd.read_csv('/home/brianc/ds/metis/prework/dsp/python/faculty.csv')
+faculty.columns = ['name','degree','title','email']
+faculty["degree"] =faculty["degree"].str.strip().str.replace('Ph.D.','PhD').str.replace('Ph.D','PhD').str.replace('PhD', 'Ph.D.')
+faculty["degree"]=faculty["degree"].str.strip().str.replace('ScD','Sc.D.').str.replace('MS','M.S.')
+firstLast =(r'(.+)\s(.+)')
+faculty[['FirstName','LastName']]=faculty['name'].str.extract(firstLast, expand = True)
+faculty.drop(['name'], axis=1, inplace=True)
+faculty = faculty[['LastName','FirstName','degree','title','email']]
+faculty = faculty.sort(['LastName','FirstName'])
+mydict = faculty.set_index(['LastName','FirstName']).T.to_dict()
+
+first3pairs = {k: mydict[k] for k in sorted(mydict.keys())[:3]}
 first3pairs

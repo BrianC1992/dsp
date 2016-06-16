3f3c7f9cb8ecaf812638446fc8731b474aefd847
BrianC1992 <BrianC1992@users.noreply.github.com>
2016-06-15 22:59:54 -0400

    Update advanced_python_dict.py

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -17,3 +17,21 @@ for x in range(len(faculty)):
     mydict[currentid].append(currentvalue)
 first3pairs = {k: mydict[k] for k in list(mydict.keys())[:3]}
 first3pairs
+
+#Question 7
+import pandas as pd
+import numpy as np
+import re
+
+faculty = pd.read_csv('/home/brianc/ds/metis/prework/dsp/python/faculty.csv')
+faculty.columns = ['name','degree','title','email']
+faculty["degree"] =faculty["degree"].str.strip().str.replace('Ph.D.','PhD').str.replace('Ph.D','PhD').str.replace('PhD', 'Ph.D.')
+faculty["degree"]=faculty["degree"].str.strip().str.replace('ScD','Sc.D.').str.replace('MS','M.S.')
+firstLast =(r'(.+)\s(.+)')
+faculty[['FirstName','LastName']]=faculty['name'].str.extract(firstLast, expand = True)
+faculty.drop(['name'], axis=1, inplace=True)
+faculty = faculty[['FirstName','LastName','degree','title','email']]
+mydict = faculty.set_index(['FirstName','LastName']).T.to_dict()
+      
+first3pairs = {k: mydict[k] for k in list(mydict.keys())[:3]}
+first3pairs
